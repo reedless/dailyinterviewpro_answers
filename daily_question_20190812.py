b99860922ec7953fa737f6c3403252af223255ab
HowardYang97 <howardyanghaowen@gmail.com>
2019-08-13 17:37:23 +0800

    12 Aug

--- daily_question_20190812.py
+++ daily_question_20190812.py
@@ -9,35 +9,27 @@ Output: -4
 
 def evalHelper(exp):
     if (len(exp) == 0):
-        print("wthhhhhhhhhhhh")
-        return
-    c = exp[0]
-    rest = exp[1:]
-    print(exp)
-    print(c)
+        return 0
     if (len(exp) == 1):
-        return int(c)
+        return int(exp)
+
+    c = exp[0]
     if (c == '+'):
-        return evalHelper(rest)
-    elif (c == '-'):
-        return -(evalHelper(rest))
-    elif (c == '('):
+        return eval(exp[1:])
+    if (c == '-'):
+        return -(eval(exp[1:]))
+    if (c == '('):
         count = 0
-        i = 0
-        for char in exp:
-            if (char == '('):
+        for i in range(len(exp)):
+            if (exp[i] == '('):
                 count += 1
-            if (char == ')'):
+            if (exp[i] == ')'):
                 count -= 1
             if (count == 0):
-                break
-            i += 1
-            print(i)
-        print(exp[1:i])
-        return evalHelper(exp[1:i]) + evalHelper(exp[i+2:])
-    else:
-        print('else')
-        return int(c) + evalHelper(rest)
+                return eval(exp[1:i]) + eval(exp[i+1:])
+        print("Invalid expression, missing closing parenthesis")
+        return 0
+    return int(c) + eval(exp[1:])
 
 
 def eval(expression):
@@ -46,3 +38,5 @@ def eval(expression):
 
 print (eval('- ( 3 + ( 2 - 1 ) )'))
 # -4
+print (eval('3 + ( 5 - 2 ) - ( 7 - 4 )'))
+# 3

5930c8efeac5dd2bc515f7e866e7f932d1f1b260
HowardYang97 <howardyanghaowen@gmail.com>
2019-08-19 15:54:13 +0800

    17 Aug done

--- 2019_08/daily_question_20190817.py
+++ 2019_08/daily_question_20190817.py
@@ -1,7 +1,7 @@
 '''
 You are given 2 integers n and m representing an n by m grid,
 determine the number of ways you can get from the top-left to the bottom-right
-of the matrix y going only right or down.
+of the matrix by going only right or down.
 
 Example:
 n = 2, m = 2
@@ -12,7 +12,17 @@ Down, right.
 '''
 
 def num_ways(n, m):
-    # Fill this in.
+    right = n-1
+    down = m-1
+    num = 1
+    deno = 1
 
-print num_ways(2, 2)
+    for i in range(right, 0, -1):
+        num = num * (i + down)
+        deno = deno * i
+    return int(num/deno)
+
+print(num_ways(2, 2))
 # 2
+print(num_ways(3, 3))
+# 6
